c319bcd6d7999ac37e4e1b194491f712d0f3e029
AROKIA SUKINAJAN A <arokiasukinrajana@kyyba.com>
2023-07-17 14:46:16 +0530

    commit

--- todo.js
+++ todo.js
@@ -12,8 +12,13 @@ function create() {
             completed: completed
         };
 
-        todoList.push(todoItem);
-        console.log(todoItem)
+        var existingItem = todoList.find(item => item.id === id);
+        if (existingItem) {
+            existingItem.task = task;
+            existingItem.completed = completed;
+        } else {
+            todoList.push(todoItem);
+        }
 
         //to clear the old given values
         idInput.value = "";
@@ -22,26 +27,33 @@ function create() {
     }
 }
 
-function read() {
+function read(filteredList) {
     var ul = document.getElementById("todoUList");
     ul.innerHTML = "";
 
-    todoList.forEach(todoItem => {
+    var listToDisplay = filteredList || todoList; // Use filteredList if !null, else use todoList
+
+    listToDisplay.forEach(todoItem => {
         var li = document.createElement("li");
         li.innerText = `ID: ${todoItem.id}, Task: ${todoItem.task}, Completed: ${todoItem.completed}`;
-        
+
+        var editButton = document.createElement("button");
+        editButton.innerText = "Edit";
+        editButton.addEventListener("click", function () {
+            editTodoItem(todoItem.id);
+        });
+
         var deleteButton = document.createElement("button");
         deleteButton.innerText = "Delete";
         deleteButton.addEventListener("click", function () {
             deleteTodoItem(todoItem.id);
-            ul.removeChild(li);
+            li.remove();
         });
         li.appendChild(deleteButton);
+        li.appendChild(editButton);
         ul.appendChild(li);
     });
-    
 }
-
 
 function deleteTodoItem(id) {
     todoList = todoList.filter(todoItem => todoItem.id !== id);
@@ -50,10 +62,10 @@ function deleteTodoItem(id) {
 function clearAll() {
     todoList = [];
     var ul = document.getElementById("todoUList");
-    ul.innerHTML = ""; 
-  }
+    ul.innerHTML = "";
+}
 
-  function sortTodoList() {
+function sortTodoList() {
     var sortSelect = document.getElementById("sortSelect");
     var sortValue = sortSelect.value;
 
@@ -65,34 +77,35 @@ function clearAll() {
     read();
 }
 
-
 function filterTodoList() {
     var filterSelect = document.getElementById("completedFilter");
     var filterValue = filterSelect.value;
-
-    var ul = document.getElementById("todoUList");
-    ul.innerHTML = ""; 
 
     if (filterValue === "all") {
         read();
     } else {
         var completedStatus = filterValue === "true";
         var filteredList = todoList.filter(todoItem => todoItem.completed === completedStatus);
-        filteredList.forEach(todoItem => {
-            var li = document.createElement("li");
-            li.innerText = `ID: ${todoItem.id}, Task: ${todoItem.task}, Completed: ${todoItem.completed}`;
-
-            var deleteButton = document.createElement("button");
-            deleteButton.innerText = "Delete";
-            deleteButton.addEventListener("click", function () {
-                deleteTodoItem(todoItem.id);
-                ul.removeChild(li);
-            });
-            li.appendChild(deleteButton);
-            ul.appendChild(li);
-        });
+        read(filteredList);
     }
 }
 
+function search() {
+    var searchTask = document.getElementById("searchInput");
+    var searchValue = searchTask.value
+    var searchedList = todoList.filter(todoItem => todoItem.task === searchValue);
+    read(searchedList);
+}
 
+function editTodoItem(id) {
+    var editItem = todoList.find(todoItem => todoItem.id === id);
+    if (editItem) {
+        var idInput = document.getElementById("idInput");
+        var taskInput = document.getElementById("taskInput");
+        var completedInput = document.getElementById("completedInput");
 
+        idInput.value = editItem.id;
+        taskInput.value = editItem.task;
+        completedInput.checked = editItem.completed;
+    }
+}
